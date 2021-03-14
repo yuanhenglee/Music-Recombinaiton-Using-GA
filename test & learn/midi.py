3c1ad4e02c8c34b82c20115192965e14b00b8074
yuanhenglee <1990henrylee@gmail.com>
2021-03-15 00:23:56 +0800

    init

---
+++ test & learn/midi.py
@@ -0,0 +1,83 @@
+from mido import MidiFile, Message
+from math import gcd
+import numpy as np
+
+class eventPair:
+    def __init__( self, firstEvent, secondEvent ):
+        self.firstEvent = firstEvent
+        self.secondEvent = secondEvent
+
+# check if two events can possiblely be a pair
+def isPair( a, b ):
+    if a.channel == b.channel and a.note == b.note :
+        return True;
+    return False;
+
+def findPeriodGCD( mid ):
+
+    timeSet = [] # store possible periods
+    for i, track in enumerate(mid.tracks):
+        for j,firstEvent in enumerate(track):
+            if firstEvent.time > 0:
+                timeSet.append(firstEvent.time) 
+
+    # drop first event's time
+    timeSet.pop(0)
+
+    return gcd(*timeSet) 
+
+
+def parseMIDI( mid ):
+
+    # Constant
+    MAXNOTE = 100
+    MINLENGTH = findPeriodGCD( mid )
+
+    # initialize
+    noteSeq = np.zeros(MAXNOTE)
+    curTime = -1
+
+    for i, track in enumerate(mid.tracks):
+        print('Track {}: {}'.format(i, track.name))
+        for j,firstEvent in enumerate(track):
+
+            # GOAL:
+            # - check if monophonic
+            # - find gcd among "time"
+            # - convert events to notes sequence on a timeline  
+
+            if firstEvent.type == "note_on" and curTime == -1: curTime = 0 # first note-on
+            else: curTime += firstEvent.time
+
+            if ( firstEvent.type == "note_on" ):
+                # find note_off event for this one
+                deltaTime = 0
+                pairFound = False
+                for secondEvent in track[j+1:-1]:
+                    deltaTime = deltaTime + secondEvent.time;
+                    if secondEvent.type == "note_off" and isPair (firstEvent,secondEvent):
+
+                        # test
+                        print(firstEvent)
+                        print(secondEvent)
+                        print("delta time = " + str(deltaTime) )
+                        print("current Time = " + str(curTime) + "\n---\n" )
+                        # test
+
+
+
+                        
+                        pairFound = True
+
+                        break;
+                if not pairFound:
+                    raise ValueError("Input notes can't be paired. QQ~")
+                    
+
+
+
+
+if __name__ == "__main__":
+    mid = MidiFile("C:/Users/user/Documents/code/Music Recombination/test & learn/test1.mid")
+    print("??")
+    parseMIDI( mid )

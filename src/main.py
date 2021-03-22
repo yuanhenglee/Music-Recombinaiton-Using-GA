4b615c6c199504f329d3cd3beb63fb325b6814e2
grace0950 <pinyan0950@gmail.com>
2021-03-22 19:48:42 +0800

    fix relative path

--- src/main.py
+++ src/main.py
@@ -6,10 +6,10 @@ from mido import MidiFile
 
 if __name__ == "__main__":
 
-        mid = MidiFile("C:/Users/user/Documents/code/Music Recombination/midi_file/young.mid")
+    mid = MidiFile("../midi_file/young.mid")
 
-        period = ProcessedMIDI( mid)
+    period = ProcessedMIDI(mid)
 
-        period.printPeriod()
+    period.printPeriod()
 
-        LBDM( period )
+    LBDM(period)

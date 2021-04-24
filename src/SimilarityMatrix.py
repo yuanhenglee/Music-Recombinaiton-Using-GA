72b92884c3fe8bc91890a14922d67f2717dce73a
grace0950 <pinyan0950@gmail.com>
2021-04-25 00:39:52 +0800

    update Simlarity Matrix

--- src/SimilarityMatrix.py
+++ src/SimilarityMatrix.py
@@ -13,24 +13,47 @@ pd.set_option('display.width', 2000)
 pd.set_option('display.float_format', '{:8,.2f}'.format)
 
 
+def pitch_func(x, y):
+    x = np.asarray(x)
+    m = x.shape[0]
+    y = np.asarray(y)
+    n = y.shape[0]
+
+    result = np.empty([m, n])
+    for i in range(m):
+        for j in range(n):
+            result[i][j] = 1 if x[i] == y[j] else 0
+    return result
+
+
 def similarityMatrix(target):
 
-    DF_var = pd.DataFrame.from_dict({
-        "Pitch": target.noteSeq[C.PITCHINDEX],
+    Interval_var = pd.DataFrame({
         "Interval": target.noteSeq[C.INTERVALINDEX]
     })
 
-    DF_var.index = np.array([Utility.value2Pitch(i)
-                             for i in target.noteSeq[C.PITCHINDEX]])
+    Interval_var.index = np.array([Utility.value2Pitch(i)
+                                   for i in target.noteSeq[C.PITCHINDEX]])
 
-    DF_SM = pd.DataFrame(1/(1+distance_matrix(DF_var, DF_var)),
-                         columns=DF_var.index, index=DF_var.index)
+    Pitch_var = pd.DataFrame({
+        "Pitch": target.noteSeq[C.PITCHINDEX]
+    })
 
-    print(DF_var.T)
-    print(DF_SM)
+    Pitch_var.index = np.array([Utility.value2Pitch(i)
+                                for i in target.noteSeq[C.PITCHINDEX]])
+
+    DF_Interval = pd.DataFrame(1/(1+distance_matrix(Interval_var, Interval_var)),
+                               columns=Interval_var.index, index=Interval_var.index)
+
+    DF_Pitch = pd.DataFrame(pitch_func(Pitch_var, Pitch_var),
+                            columns=Pitch_var.index, index=Pitch_var.index)
+
+    DF_Combine = (DF_Interval + DF_Pitch) / 2
+
+    print(DF_Combine)
 
     # Display DF_SM as a matrix in a new figure window
-    plt.matshow(DF_SM)
+    plt.matshow(DF_Combine)
     # Set the colormap to 'bone'.
     plt.bone()
     plt.show()

ec03ff2f3276e3348573e709a431e4ab630b693a
grace0950 <pinyan0950@gmail.com>
2021-06-14 19:56:10 +0800

    store music

--- src/zodb_store.py
+++ src/zodb_store.py
@@ -1,8 +1,8 @@
 from zodb import MyZODB, transaction
 from music import Music
 
-db = MyZODB('./Music/Data.fs')
+db = MyZODB('./Music/刻在我心底的名字.fs')
 dbroot = db.dbroot
-dbroot['Full Stop'] = Music('../midi_file/FullStop.mid', 'Full Stop')
+dbroot['刻在我心底的名字'] = Music('../midi_file/刻在我心底的名字.mid', '刻在我心底的名字')
 transaction.commit()
 db.close()

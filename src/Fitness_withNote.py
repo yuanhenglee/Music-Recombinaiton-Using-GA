171847c217e145c731b84c67a30d911eeb2590a2
grace0950 <pinyan0950@gmail.com>
2021-11-17 17:22:52 +0800

    GA with note

---
+++ src/Fitness_withNote.py
@@ -0,0 +1,242 @@
+import numpy as np
+import pandas as pd
+import math
+import Feature
+
+import Constant as C
+
+
+def musicSourceVariety(individual):
+    sum_of_duration1 = 0
+    sum_of_duration2 = 0
+    sum_of_duration_mutated = 0
+    for tree in individual.tree_list:
+        if tree.id == C.INPUT_NAMES[0]:
+            sum_of_duration1 += tree.length
+        elif tree.id == C.INPUT_NAMES[1]:
+            sum_of_duration2 += tree.length
+        elif tree.id == "Mutated":
+            sum_of_duration_mutated += tree.length
+        # else:
+        #     print(tree.id)
+        #     print(C.INPUT_NAMES)
+        #     raise ValueError("Wrong tree id")
+
+    sum_of_duration = sum_of_duration1+sum_of_duration2
+    # print(f"{sum_of_duration=}")
+    # print(f"{sum_of_duration1=}")
+    # print(f"{sum_of_duration2=}")
+    # print(f"{sum_of_duration_mutated=}")
+    if sum_of_duration == 0:
+        return 1
+    result = abs(C.INPUT_RATE - sum_of_duration1/sum_of_duration)
+
+    return result
+
+
+def calculateSimilarity(value, ancestors_value):
+    return abs(ancestors_value-value)
+
+
+def calculateConsensus(value):
+    return abs(value)
+
+
+def calculateLongNote(individual):
+    long_note_score = []
+    parsedMIDI = individual.parsedMIDI
+    valid_note_1 = [1.0, 2.0, 3.0]
+    valid_note_2 = [7.0, 6.0, 5.0]
+    for i in range(parsedMIDI.numberOfNotes):
+        if parsedMIDI.noteSeq[C.DURATIONINDEX][i] > max(4, np.percentile(parsedMIDI.noteSeq[C.DURATIONINDEX], 75)):
+            pitch = (float(parsedMIDI.noteSeq[C.PITCHINDEX][i] % 7))
+            if pitch in valid_note_1:
+                long_note_score.append(0)
+            elif pitch in valid_note_2:
+                long_note_score.append(1)
+            else:
+                long_note_score.append(5)
+    if len(long_note_score) > 0:
+        long_note_score = np.mean(long_note_score)
+    else:
+        long_note_score = 0
+
+    return long_note_score
+
+
+def calculateInRange(max, min, value):
+    if value <= max and value >= min:
+        return 0
+    return 1
+
+
+def elementsTransition(individual):
+    first_note = individual.tree_list[0].pitchSeq[-1]
+    if first_note == 0 and len(individual.tree_list[0].pitchSeq) > 1:
+        first_note = individual.tree_list[0].pitchSeq[-2]
+    each_score = []
+    for tree in individual.tree_list[1:]:
+        second_note = tree.pitchSeq[0]
+        if second_note == 0 and len(tree.pitchSeq) > 1:
+            second_note = tree.pitchSeq[1]
+        interval = abs(second_note - first_note)
+        if first_note == 0 or second_note == 0:
+            interval = 0.0
+        if interval in [0.0, 1.0, 2.0]:
+            each_score.append(0)
+        elif interval in [3.0, 4.0]:
+            each_score.append(1)
+        elif interval in [5.0, 6.0, 7.0]:
+            each_score.append(10)
+        else:
+            each_score.append(20)
+        first_note = tree.pitchSeq[-1]
+        if first_note == 0 and len(tree.pitchSeq) > 1:
+            first_note = tree.pitchSeq[-2]
+
+        # print(tree.pitchSeq)
+        # print(interval)
+
+    if len(each_score) > 0:
+        return np.mean(each_score)
+    else:
+        return 0
+
+
+def tailScore(individual):
+    last_pitch = individual.parsedMIDI.noteSeq[C.PITCHINDEX][-1]
+    last_duration = individual.parsedMIDI.noteSeq[C.DURATIONINDEX][-1]
+    last_2_pitch = individual.parsedMIDI.noteSeq[C.PITCHINDEX][-2]
+    if last_pitch == 0:
+        last_pitch = individual.parsedMIDI.noteSeq[C.PITCHINDEX][-2]
+        last_duration = individual.parsedMIDI.noteSeq[C.DURATIONINDEX][-2]
+        last_2_pitch = individual.parsedMIDI.noteSeq[C.PITCHINDEX][-3]
+
+    if float(last_pitch) % 7 in [1.0]:
+        score_last_pitch = 0
+    elif float(last_pitch) % 7 in [2.0, 5.0, 6.0]:
+        score_last_pitch = 1
+    else:
+        score_last_pitch = 5
+
+    duration_ratio = float(last_duration) / \
+        np.mean(individual.parsedMIDI.noteSeq[C.DURATIONINDEX])
+    if duration_ratio >= 2:
+        score_last_duration = 0
+    elif duration_ratio >= 1:
+        score_last_duration = 1
+    else:
+        score_last_duration = 5
+
+    interval = abs(last_pitch - last_2_pitch)
+    if interval in [0.0, 1.0, 2.0]:
+        score_interval = 0
+    else:
+        score_interval = 5
+
+    return score_last_pitch + score_last_duration + score_interval
+
+
+def updateFitness(individual):
+    individual.fitness = 0
+
+    # fitness score initialize
+    similarity = 0
+    consensus = 0
+    inRange = 0
+
+    # path_orderedData = "../test & learn/EDA Result/songMeanSTD_ordered.csv"
+    path_Data = "../test & learn/EDA Result/songMeanSTD.csv"
+    data = pd.read_csv(path_Data)
+    # orderedData = pd.read_csv(path_orderedData)
+    # data.rename(columns={"Unnamed: 0": "feature"}, inplace=True)
+    # orderedData.rename(columns={"Unnamed: 0": "feature"}, inplace=True)
+
+    # similarity
+    similarity = 0
+    for ancestor in individual.ancestor:
+        df_tmp = C.similarity_weight * \
+            (ancestor.df_features_std - individual.df_features_std)
+        similarity += df_tmp.abs().to_numpy().sum()
+    # similarity_score = np.zeros(C.NUMBER_FEATURES)
+
+    # for ancestor in ancestors:
+    #     for i in range(len(similarity_score)):
+    #         similarity_score[i] = calculateSimilarity(
+    #             individual.df_features_std.iloc[0, i], ancestor.df_features_std.iloc[0, i])
+    #     similarity += similarity_score.mean()
+    # similarity = similarity/len(ancestors)
+
+    # consensus
+    df_consensus_score = C.consensus_weight * individual.df_features_std
+    consensus = df_consensus_score.abs().to_numpy().sum()
+    # consensus += 3 * elementsTransition(individual)
+    consensus += calculateLongNote(individual)
+    consensus += tailScore(individual)
+
+    # consensus_score = np.zeros(10)
+
+    # for i in range(len(consensus_score)):
+    #     featureName = orderedData["feature"][i]
+    #     index = data[data["feature"] == featureName].index.values[0]
+    #     consensus_score[i] = calculateConsensus(
+    #         individual.df_features_std.iloc[0, index])
+
+    # consensus += (consensus_score.mean())
+
+    # inRange
+    inRange_score = np.zeros(C.NUMBER_FEATURES)
+
+    for i in range(len(inRange_score)):
+        inRange_score[i] = calculateInRange(
+            data["max"][i], data["min"][i], individual.df_features.iloc[0, i])
+
+    inRange += (inRange_score.mean())
+
+    # music Source Variety
+    # music_source_variety = musicSourceVariety(individual)
+
+    # print("similarity: ", similarity)
+    # print("consensus: ", consensus)
+    # print("inRange: ", inRange)
+
+    # n_tree = len(individual.tree_list)
+    # if 10 <= n_tree <= 16:
+    #     n_tree_score = 0
+    # elif 10 > n_tree:
+    #     n_tree_score = abs(n_tree - 10)
+    # elif n_tree > 16:
+    #     n_tree_score = abs(n_tree - 16)
+
+    # n tree score
+    # n_invalid_tree = 0
+    # for tree in individual.tree_list:
+    #     if 1/16 <= tree.length/individual.parsedMIDI.totalDuration:  # <= 1/10:
+    #         ...
+    #     else:
+    #         n_invalid_tree += 1
+    # n_tree_score = n_invalid_tree/len(individual.tree_list)
+
+    # # element alternate rate
+    # n_element_alternate = 0
+    # if len(individual.tree_list) <= 1:
+    #     element_alternate_score = 1
+    # else:
+    #     cur_tree_id = individual.tree_list[0].id
+    #     for tree in individual.tree_list:
+    #         if tree.id != cur_tree_id:
+    #             n_element_alternate += 1
+    #             cur_tree_id = tree.id
+    #     element_alternate_score = 1 - n_element_alternate / \
+    #         (len(individual.tree_list)-1)
+
+    individual.fitness_detail = [similarity,
+                                 consensus, inRange]
+
+    for i in range(len(individual.fitness_detail)):
+        individual.fitness_detail[i] = C.FITNESS_WEIGHT[i] * \
+            individual.fitness_detail[i]
+
+    individual.fitness = sum(individual.fitness_detail)
+
+    # print(individual.fitness_detail)
---
+++ src/GeneticAlgorithm_withNote.py
@@ -0,0 +1,272 @@
+from hashlib import new
+import sys
+import os
+import time
+from mido import MidiFile
+from Feature import main
+import Preprocess
+import MusicSegmentation_2
+from Individual import Individual
+from Fitness_withNote import updateFitness
+from ILBDM import ILBDM, ILBDM_elementary_noteSeq
+import Utility
+import Constant as C
+
+import numpy as np
+import random
+from zodb import ZODB, transaction
+import copy
+import Demo
+import MusicTree
+
+
+def startGA(initialAncestors, population, mutation_rate=0.3, crossover_rate=0.3, max_generation=1000, max_population=1000, n_generation_terminate=100, generation_to_kill=10):
+    best_score = 99999999999
+    generation_best_score_was_born = 1
+    for generation in range(1, max_generation+1):
+        # Measuring the fitness of each chromosome in the population.
+        for individual in population:
+            updateFitness(individual)
+
+        if generation >= generation_to_kill and len(population) > max_population:
+            population.sort(key=lambda x: x.fitness)
+            tmp_population = []
+            key_fitness = 0
+            for individual in population:
+                if individual.fitness != key_fitness:
+                    key_fitness = individual.fitness
+                    tmp_population.append(individual)
+            population = tmp_population
+
+        population = natural_selection(population, max_population)
+
+        for i in population:
+            if i.isAncestor != True:
+                if i.fitness < best_score:
+                    best_score = i.fitness
+                    generation_best_score_was_born = generation
+                break
+
+        # ! TEST ONLY
+        print("==================\ngeneration: \n", generation)
+
+        for individual in population:
+            if individual.isAncestor == True:
+                print("*", end="")
+            else:
+                print(" ", end="")
+
+            print(round(individual.fitness, 6))
+        # print("\n", population[0].parsedMIDI.noteSeq[C.INTERVALINDEX])
+        # ! TEST ONLY
+
+        ''' terminate '''
+        if population[0] == 0 or generation - generation_best_score_was_born >= n_generation_terminate:
+            print("GA Terminate")
+            break
+
+        ''' Generating next generation using crossover. '''
+        population += crossover(population, initialAncestors,
+                                n_offspring=crossover_rate * max_population)
+        # TODO control how many individuals will be mutated
+        ''' Adding some variations to the offspring using mutation. '''
+        population += mutation(population, initialAncestors,
+                               int(mutation_rate * max_population))
+
+    return population
+
+
+def natural_selection(population, n_selected_goal):
+    ''' Selecting the best individuals in the current generation for the next generation. '''
+    if len(population) <= n_selected_goal:
+        return population
+    else:
+        population.sort(key=lambda x: x.fitness)
+        ancestors = []
+        for i in population[n_selected_goal:]:
+            if i.isAncestor:
+                ancestors.append(i)
+        return population[0:n_selected_goal] + ancestors
+
+
+def crossover(parents, initialAncestors, n_offspring):
+    offspring = []
+    n_times_trying = 0
+    # stop when offspring size satisfies the criteria or either the n_times_trying exceed
+    while len(offspring) < n_offspring and n_times_trying < 100:
+        main_parent, sub_parent = random.choices(parents, k=2)
+        n_times_trying += 1
+        if main_parent == sub_parent:
+            continue
+
+        ''' Crossover '''
+        # crossover
+        # construct offspring based on main_parent
+        child = copy.deepcopy(main_parent)
+        child.isAncestor = False
+        child.ancestor = initialAncestors
+
+        main_parent_length = main_parent.parsedMIDI.numberOfNotes
+        # print("main_parent_length: ", main_parent_length)
+        sub_parent_length = sub_parent.parsedMIDI.numberOfNotes
+        # print("sub_parent_length: ", sub_parent_length)
+        child_noteSeq = child.parsedMIDI.noteSeq
+        elementarySequence = np.vstack(
+            [child_noteSeq[C.PITCHINDEX], child_noteSeq[C.DURATIONINDEX]])
+        mask = np.random.randint(2, size=main_parent_length)
+        # print("mask: ", mask)
+        # print("before pitch: ", child_noteSeq[C.PITCHINDEX])
+        for i in range(main_parent_length):
+            if mask[i] == 1:
+                elementarySequence[0][i] = sub_parent.parsedMIDI.noteSeq[C.PITCHINDEX][i %
+                                                                                       sub_parent_length]
+                elementarySequence[1][i] = sub_parent.parsedMIDI.noteSeq[C.DURATIONINDEX][i %
+                                                                                          sub_parent_length]
+        child.parsedMIDI.noteSeq = Preprocess.expandElementarySequence(
+            elementarySequence)
+        # print("after pitch: ", child.parsedMIDI.noteSeq[C.PITCHINDEX])
+
+        child.parsedMIDI.updateFieldVariable()
+        child.calculateAllFeatures()
+
+        # main_parent.details()
+        # child.details()
+
+        offspring.append(child)
+    offspring.sort(key=lambda x: x.fitness, reverse=True)
+    return offspring
+
+# TODO preserve the original individual
+
+
+def mutation(parents, initialAncestors, mutation_size):
+    newPopulation = []
+
+    selected_parents = random.sample(parents, mutation_size)
+
+    for main_parent in selected_parents:
+
+        child = copy.deepcopy(main_parent)
+        child.isAncestor = False
+        child.ancestor = initialAncestors
+
+        child_length = child.parsedMIDI.numberOfNotes
+        child_noteSeq = child.parsedMIDI.noteSeq
+        elementarySequence = np.vstack(
+            [child_noteSeq[C.PITCHINDEX], child_noteSeq[C.DURATIONINDEX]])
+        mask = np.random.choice(2, child_length, p=[0.8, 0.2])
+        # print("mask: ", mask)
+        # print("before pitch: ", child_noteSeq[C.PITCHINDEX])
+        for i in range(child_length):
+            if mask[i] == 1:
+                is_negative = np.random.randint(2)
+                move = 3
+                if is_negative == 1:
+                    move *= -1
+                elementarySequence[0][i] += move
+
+        child.parsedMIDI.noteSeq = Preprocess.expandElementarySequence(
+            elementarySequence)
+        # print("after pitch: ", child.parsedMIDI.noteSeq[C.PITCHINDEX])
+
+        child.parsedMIDI.updateFieldVariable(child.parsedMIDI)
+        child.calculateAllFeatures()
+        newPopulation.append(child)
+
+    return newPopulation
+
+
+def findBestOffspring(population):
+    population.sort(key=lambda x: x.fitness)
+    for i in population:
+        if i.isAncestor != True:
+            return i
+    return None
+
+
+def selectRandomElement(individual):
+    # selected element can not be signature
+    element_number_set = copy.deepcopy(individual.allElementGroups)
+    element_number_set.remove(individual.signature)
+    selected_element_number = random.sample(element_number_set, 1)[0]
+    point = []
+    tree_sublist = []
+    start = -1
+    element_number_list = individual.parsedMIDI.noteSeq[C.ELEMENTINDEX]
+    for i, number in enumerate(element_number_list):
+        if number == selected_element_number and start == -1:
+            start = i
+        elif number != selected_element_number and start != -1:
+            end = i
+            point.append((start, end))
+            start = -1
+        if start != -1 and i == len(element_number_list)-1:
+            end = i+1
+            point.append((start, end))
+
+    # finding tree been select
+    acc_tree_index = 0
+    duration_before = sum(
+        [i for i in individual.parsedMIDI.noteSeq[C.DURATIONINDEX][:point[0][0]]])
+    duration_after = sum(
+        [i for i in individual.parsedMIDI.noteSeq[C.DURATIONINDEX][:point[0][1]]])
+
+    for tree in individual.tree_list:
+        acc_tree_index += tree.length
+        if(tree_sublist == [] and duration_before < acc_tree_index):
+            tree_sublist.append(tree)
+        if(duration_before <= acc_tree_index <= duration_after):
+            tree_sublist.append(tree)
+
+    if len(tree_sublist) == 1:
+        selected_id = tree_sublist[0].id
+    else:
+        if tree_sublist[0].id == tree_sublist[1].id:
+            selected_id = tree_sublist[0].id
+        else:
+            selected_id = "Mutated"
+
+    return point, selected_id
+
+
+if __name__ == "__main__":
+
+    try:
+        paths = sys.argv[1:3]
+    except:
+        print("Missing input MIDI file!")
+
+    try:
+        rate = sys.argv[3]
+    except:
+        rate = 0.5
+    population = []
+    initialAncestors = []
+
+    for path in paths:
+        db = ZODB(path)
+        dbroot = db.dbroot
+        initialAncestors.append(dbroot[0])
+        population.append(dbroot[0])
+        db.close()
+        string_list = path.split("/")
+        name = string_list[-1]
+        C.INPUT_NAMES.append(name[:-3])
+    C.INPUT_RATE = float(rate)
+    # print(ids)
+    new_population = startGA(initialAncestors, population,
+                             max_population=30, max_generation=1000)
+    bestOffspring = findBestOffspring(new_population)
+    # bestOffspring = population[0]
+
+    if bestOffspring != None:
+        bestOffspring.parsedMIDI.printMIDI()
+        mid_output = Demo.parsed2MIDI(bestOffspring.parsedMIDI)
+        cur_time = str(time.strftime('%m_%d_%H_%M', time.localtime()))
+        mid_output.save('../output/' + cur_time + ".mid")
+        os.makedirs("../output/" + cur_time)
+        dbpath = "../output/" + cur_time + "/" + cur_time + ".fs"
+        db = ZODB(dbpath)
+        db.dbroot["Individual"] = bestOffspring
+        transaction.commit()
+        db.close()
